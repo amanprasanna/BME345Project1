8dae80596b318dde42fbea971a66185d032a3168
jfacche <jfacche@ncsu.edu>
2025-02-17 15:23:01 -0500

    Update BME345Project1.m

--- BME345Project1.m
+++ BME345Project1.m
@@ -83,20 +83,20 @@ al4 = ans(6);
 
 % Finding the x and y positions and al components based on th2, th3, and th4
 for k = 1:length(th2new)
-    r12x(k) = -(r2/2).*cosd(th2new(k)); 
-    r12y(k) = -(r2/2).*sind(th2new(k)); 
-    r32x(k) = (r2/2).*cosd(th2new(k)); 
-    r32y = (r2/2).*sind(th2new(k)); 
+    r12x(k) = -(r2/2).*cos(th2new(k)); 
+    r12y(k) = -(r2/2).*sin(th2new(k)); 
+    r32x(k) = (r2/2).*cos(th2new(k)); 
+    r32y(k) = (r2/2).*sin(th2new(k)); 
     
-    r23x(k) = -(r3/2).*cos(th3(k)); % Neg due to vector being neg in x and y plane
-    r23y(k) = -(r3/2).*sin(th3(k)); 
-    r43x(k) = (r3/2).*cos(th3(k)); 
-    r43y = (r3/2).*sin(th3(k)); 
+    r23x(k) = -(r3*lowerlegDistal).*cos(th3(k)); % lower leg cannot be divided by 2
+    r23y(k) = -(r3*lowerlegDistal).*sin(th3(k)); 
+    r43x(k) = (r3*lowerlegDistal).*cos(th3(k)); 
+    r43y(k) = (r3*lowerlegDistal).*sin(th3(k)); 
 
-    r34x(k) = -(r4/2).*cos(th4(k)); % Neg due to ang has opp direction than vector
-    r34y(k) = -(r4/2).*sin(th4(k));
-    r14x(k) = (r4/2).*cos(th4(k)); 
-    r14y(k) = (r4/2).*sin(th4(k));
+    r34x(k) = -(r4*thighDistal).*cos(th4(k)); % thigh cannot be divided by 2 
+    r34y(k) = -(r4*thighDistal).*sin(th4(k));
+    r14x(k) = (r4*thighDistal).*cos(th4(k)); 
+    r14y(k) = (r4*thighDistal).*sin(th4(k));
 
     al2x(k) = 0 - (om2new(k).^2).*(r2/2).*cos(th2new(k)); 
     al2y(k) = 0 - (om2new(k).^2).*(r2/2).*sin(th2new(k)); 
@@ -108,6 +108,38 @@ for k = 1:length(th2new)
     + (-al4(k).*(r4/2).*sin(th4(k)) - (om4(k).^2)*(r4/2).*cos(th4(k))); 
     al4y(k) = (0 - (om2new(k).^2).*(r2/2).*sin(th2new(k))) + (al3(k).*(r3/2).*cos(th3(k)) - (om3(k)^2).*(r3/2).*sin(th3(k))) ...
     + (al4(k).*(r4/2).*cos(th4(k)) - (om4(k).^2).*(r4/2).*sin(th4(k))); 
+
+end
+
+% Solving for the Force Components
+
+%  F12x  F12y  F32x F32y F23x F23y F43x F43y F34x  F34y F14x F14y  TD
+for k = 1:length(al4y)
+A = [1    0     1    0    0    0    0     0    0    0    0    0    0; 
+     0    1     0    1    0    0    0     0    0    0    0    0    0;
+ -r12y(k)' r12x(k)' -r32y(k)' r32x(k)'  0     0    0    0    0    0    0    0    1; % torque to solve is T2
+     0    0     0    0    1    0    1     0    0    0    0    0    0;
+     0    0     0    0    0    1    0     1    0    0    0    0    0;
+     0    0     0    0  -r23y(k)' r23x(k)' -r43y(k)' r43x(k)'  0    0    0    0    0;
+     0    0     0    0    0    0    0     0    1    0    1    0    0;
+     0    0     0    0    0    0    0     0    0    1    0    1    0;
+     0    0     0    0    0    0    0     0  -r34y(k)' r34x(k)' -r14y(k)' r14x(k)' 0;
+     0    0     1    0    1    0    0     0    0    0    0    0    0;
+     0    0     0    1    0    1    0     0    0    0    0    0    0;
+     0    0     0    0    0    0    1     0    1    0    0    0    0;
+     0    0     0    0    0    0    0     1    0    1    0    0    0];
+% % 
+% % %Right hand side of the equation/ known variables
+% % % Note to self: need to fix Ta
+if (any(th2new) >= pi && any(th2new) <= 3*pi/2) || (any(th2new) >= 5*pi/2 && any(th2new) <= 3*pi)
+b = [m2.*al2x(k) (m2.*al2y(k) - m2*g) (I2y*al2) m3.*al3x(k) (m3.*al3y(k) - m3*g) I3.*al3(k) m4.*al4x(k) (m4.*al4y(k) - m4*g) (I4.*al4(k) - Ta) 0 0 0 0]';
+
+else 
+b = [m2.*al2x(k) (m2.*al2y(k) - m2*g) (I2y*al2) m3.*al3x(k) (m3.*al3y(k) - m3*g) I3.*al3(k) m4.*al4x(k) (m4.*al4y(k) - m4*g) (I4.*al4(k)) 0 0 0 0]';
+end
+% % % solving the system of Equations
+F(:,k)= A\b;
+
 end
 
 
