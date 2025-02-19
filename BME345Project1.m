0edfb7a4896e1a3f43b058fabc57b06520eccc52
mlwill28 <mlwill28@ncsu.edu>
2025-02-18 20:09:55 -0500

    Update BME345Project1.m

--- BME345Project1.m
+++ BME345Project1.m
@@ -91,6 +91,50 @@ Thip(indexT0:indexT1) = 0;
 guess = [(pi/4) (pi/4) 1 1 1 1];
 options = optimoptions('fsolve','Display','final'); 
 
+% Video Info
+vidFile = 'Bike_Mirrored.mov';
+frameStart = 62; 
+frameStop = 110; 
+vid = VideoReader(vidFile);
+
+% Figure Background
+background = 'Bike_Background.png';
+
+% For cropping
+widthEdge = 700;
+widthRange = 630;
+heightEdge = 10;
+heightRange = 830;
+
+% Color Adjustments
+lthrR = 220; %red tape lower threshold
+uthrR = 255; %red tape upper threshold
+
+lthrRG = 135;%red lthr for green tape
+uthrRG = 170;%red uthr for green tape
+lthrBG = 140;%blue lthr for green tape
+uthrBG = 190;%blue uthr for green tape
+lthrGG = 220; %green tape lower threshold
+uthrGG = 255; %green tape upper threshold
+
+lthrRB = 106;%red lthr for blue tape
+uthrRB = 120;%red uthr for blue tape
+lthrGB = 155;%green lthr for blue tape
+uthrGB = 170;%green uthr for blue tape
+lthrBB = 185;%blue lthr for blue tape
+uthrBB = 210;%blue uthr for blue tape
+
+lthrGen = 110; %generalized lower threshold
+uthrGen = 225; %generalized upper threshold
+
+lthrRY = 185;%red lthr for yellow tape
+uthrRY = 210;%red uthr for yellow tape
+lthrGY = 170;%green lthr for yellow tape
+uthrGY = 190;%green uthr for yellow tape
+lthrBY = 0;%blue lthr for yellow tape
+uthrBY = 10;%blue uthr for yellow tape
+
+
 %% Calculations
 
 % Finding the th3, th4, om3, om4, al3, al4 based on initial and new guesses
@@ -212,4 +256,159 @@ ylabel("Force (N)")
 legend("Force Parallel to Thigh","Force Perpendicular to Thigh")
 
 
-%% Video Generation
+%% Determining Centroid Movement
+
+% Step through each frame 
+for k = frameStart:frameStop
+    frameSlice = read(vid,k); % loads current frame into frameSlice
+
+    frameSliceCrop = imcrop(frameSlice, ...
+        [widthEdge, heightEdge, widthRange, heightRange]);
+    red = frameSliceCrop(:,:,1);
+    green = frameSliceCrop(:,:,2);
+    blue = frameSliceCrop(:,:,3);
+    RframeSliceBW = ((red >= lthrR) & (red <= uthrR) & ...
+                    (green <= lthrGen) & (blue <= lthrGen)); 
+    GframeSliceBW = ((green >= lthrGG) & (green <= uthrGG)) & ...
+                    ((red >= lthrRG) & (red <= uthrRG)) & ...
+                    ((blue >= lthrBG) & (blue <= uthrBG));
+    BframeSliceBW = ((blue >= lthrBB) & (blue <= uthrBB)) & ...
+                    ((red >= lthrRB) & (red <= uthrRB) & ...
+                    (green >= lthrGB) & (green <= uthrGB)); 
+    YframeSliceBW = ((red >= lthrRY) & (red <= uthrRY) & ...
+                    (green >= lthrGY) & (green <= uthrGY) & ...
+                    (blue >= lthrBY) & (blue <= uthrBY));
+
+    frameSliceBW = RframeSliceBW + GframeSliceBW + BframeSliceBW + YframeSliceBW;
+    [row,col, ~] = size(frameSliceBW);
+
+    % Uses custom function centroid to return row and col of a binary image
+    [RcentRow(k), RcentCol(k)] = Centroid345(RframeSliceBW); %#ok<SAGROW>
+    [GcentRow(k), GcentCol(k)] = Centroid345(GframeSliceBW); %#ok<SAGROW>
+    [BcentRow(k), BcentCol(k)] = Centroid345(BframeSliceBW); %#ok<SAGROW>
+    [YcentRow(k), YcentCol(k)] = Centroid345(YframeSliceBW); %#ok<SAGROW>
+    
+    % Returns x and y positions of centroids
+    RxPos = RcentCol(frameStart:end);
+    RyPos = row - RcentRow(frameStart:end);
+
+    GxPos = GcentCol(frameStart:end);
+    GyPos = row - GcentRow(frameStart:end);
+
+    BxPos = BcentCol(frameStart:end);
+    ByPos = row - BcentRow(frameStart:end);
+
+    YxPos = YcentCol(frameStart:end);
+    YyPos = row - YcentRow(frameStart:end);
+
+end
+
+%% Pixel to Meter Conversion & Vector Math for Angle Guesses
+
+%Pixel position vectors
+r1Vec = [YxPos(1)-GxPos(1), YyPos(1)-GyPos(1)];
+r2Vec = [BxPos(1)-YxPos(1), ByPos(1)-YyPos(1)];
+r3Vec = [RxPos(1)-BxPos(1), RyPos(1)-ByPos(1)];
+r4Vec = [GxPos(1)-RxPos(1), GyPos(1)-RyPos(1)];
+
+%Pixel magnitudes
+r1PxMag = vecnorm(r1Vec);
+r2PxMag = vecnorm(r2Vec);
+r3PxMag = vecnorm(r3Vec);
+r4PxMag = vecnorm(r3Vec);
+
+pxmconv = [r1/r1PxMag, r2/r2PxMag, r3/r3PxMag, r4/r4PxMag];
+px_mConv = mean(pxmconv); % Conversion factor for px to m
+
+% Convert position vectors to m
+RxPos = RxPos.*px_mConv;
+RyPos = RyPos.*px_mConv;
+
+GxPos = GxPos.*px_mConv;
+GyPos = GyPos.*px_mConv;
+
+BxPos = BxPos.*px_mConv;
+ByPos = ByPos.*px_mConv;
+
+YxPos = YxPos.*px_mConv;
+YyPos = YyPos.*px_mConv;
+
+
+%% Q5: Stick Figure Video
+
+t = (length(frameStart:frameStop))/vid.FrameRate;
+    tLin = linspace(0, t, length(frameStart:frameStop));
+
+% Force at each joint in N
+% fGx = ;
+% fGy = ;
+% 
+% fRx = ;
+% fRy = ;
+% 
+% fBx = ;
+% fBy = ;
+% 
+% fYx = ;
+% fYy = ;
+
+vidObj2 = VideoWriter("cycling");
+vidObj2.FrameRate = vid.FrameRate; % set frame rate (e.g., 15)
+open(vidObj2) % opens video for recording 
+
+for k = frameStart:frameStop
+    frameSlice = read(vid,k); % loads current frame into frameSlice
+
+    frameSliceCrop = imcrop(frameSlice, ...
+        [widthEdge, heightEdge, widthRange, heightRange]);
+
+    % Images + Plot Output
+    figure(4);
+        for h = 1:(k-frameStart)      
+            plot(GxPos(1), GyPos(1), 'g.', ...
+                RxPos(h), RyPos(h), 'r.', ...
+                BxPos(h), ByPos(h), 'c.', ...
+                YxPos(1), YyPos(1), 'y.', ...
+                [GxPos(1), YxPos(1)], [GyPos(1), YyPos(1)], ...
+                [YxPos(1), BxPos(h)], [YyPos(1), ByPos(h)],  ...
+                [BxPos(h), RxPos(h)], [ByPos(h), RyPos(h)], ...
+                [RxPos(h), GxPos(1)], [RyPos(h), GyPos(1)], ...
+                1/2*(GxPos(1)+YxPos(1)), 1/2*(GyPos(1)+YyPos(1)), '.', ...
+                1/2*(YxPos(1)+BxPos(h)), 1/2*(YyPos(1)+ByPos(h)), '.', ...
+                lowerlegProximal*(BxPos(h)+RxPos(h)), lowerlegProximal*(ByPos(h)+RyPos(h)), '.', ...
+                thighProximal*(RxPos(h)+GxPos(1)), thighProximal*(RyPos(h)+GyPos(1)), '.', ...
+                RxPos, RyPos, 'r-', ...
+                'LineWidth', 1, ...
+                'MarkerSize', 20)
+            
+            % CAN ADD FORCE ARROWS HERE
+            % hold on
+            % quiver(RxPos(h), RyPos(h), fRx(h), fRy(h), .1, "Color", "r", ...
+            %     "LineWidth", 2, "MaxHeadSize", 1)
+            % quiver(GxPos(h), GyPos(h), fGx(h), fGy(h), .1, "Color", "g", ...
+            %     "LineWidth", 2, "MaxHeadSize", 1)
+            % quiver(BxPos(h), ByPos(h), fBx(h), fBy(h), .1, "Color", "c", ...
+            %     "LineWidth", 2, "MaxHeadSize", 1)
+            % quiver(YxPos(h), YyPos(h), fYx(h), fYy(h), .1, "Color", "y", ...
+            %     "LineWidth", 2, "MaxHeadSize", 1)
+            % hold off
+        end
+        title('Pedaling System')
+        xlabel('Horizontal Position (m)')
+        ylabel('Vertical Position (m)') 
+        legend('Hip', 'Knee', 'Foot', 'Bottom Bracket', 'Frame (r1)', ...
+            'Pedal (r2)', 'Leg (r3)', 'Thigh (r4)', 'r1 CoM','r2 CoM', 'r3 CoM', ...
+            'r4 CoM', 'Knee Pathway', 'Location','eastoutside')
+        xlim([0, col*px_mConv])
+        ylim([0, row*px_mConv])
+        pbaspect([1, row/col, 1])  
+        
+
+    drawnow % forces figure to appear, which may not happen in loops
+
+    frame = getframe(gcf);
+    writeVideo(vidObj2,frame); % writes frame to video
+
+end
+
+close(vidObj2) % closes video
